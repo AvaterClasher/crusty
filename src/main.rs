386b8a84670ebf7cebd85c02a203481cd794898a
AvaterClasher <avater.clasher@gmail.com>
2024-07-28 11:25:41 +0530

    feat: added lexer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{fs::File, io::{Error, Read}};
+
+use crusty::lexer::lex;
+
 fn main() {
-    println!("Hello, crusty 🦀!");
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
+    if args.len() != 1 {
+        eprintln!("Error invalid number of arguments :");
+        std::process::exit(1);
+    }
+
+    let tokens = match read_file(&args[0]) {
+        Ok(s) => lex(&s),
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    println!("{:#?}", tokens);
+}
+
+fn read_file(input: &str) -> Result<String, Error> {
+    let mut file = File::open(input)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
 }
